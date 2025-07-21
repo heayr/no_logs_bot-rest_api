70d3ae3e88fe8ec93b4d42c030c12f75fe4194f6
heayr <djtlm@yandex.ru>
2025-07-21 20:49:47 +0000

    Убрал ошибку асинхронного вызова удаления юезра

--- bot/services/full_user_removal.py
+++ bot/services/full_user_removal.py
@@ -16,8 +16,8 @@ async def full_remove_user(uuid: str) -> dict:
         # ⬇️ remove_client вызывается в отдельном потоке, так как он синхронный
         removed_from_xray = await loop.run_in_executor(None, remove_client, uuid)
 
-        # ⬇️ delete_user_by_uuid — асинхронный
-        removed_from_db = await delete_user_by_uuid(uuid)
+        # ⬇️ delete_user_by_uuid вызывается в отдельном потоке, так как он синхронный
+        removed_from_db = await loop.run_in_executor(None, delete_user_by_uuid, uuid)
 
         logging.info(f"Удаление пользователя {uuid} - Xray: {removed_from_xray}, DB: {removed_from_db}")
 
